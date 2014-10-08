c523fe7f855a2f16728fefad4ade016ff203176a
amraniyoussef <youssefamrani9@gmail.com>
2014-10-08 19:15:00 +0200

    Commit Ajout Date

--- Test3_EGIT/src/Test3_main.java
+++ Test3_EGIT/src/Test3_main.java
@@ -1,12 +1,16 @@
+import java.util.Date;
+
 
 public class Test3_main {
 
 	/**
 	 * @param args
 	 */
+	
 	public static void main(String[] args) {
 		System.out.println("Bonsoir");
-
+		Date d = new Date();
+		System.out.println(d.toString());
 	}
 
 }
